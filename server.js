9d5999ebbe28e552feefa221a5a01539b5035bba
BOULA-ROUF <boularoufgit@gmail.com>
2026-05-07 17:54:02 +0100

    update year to 2025-2026

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
